d9c446bffe4489adfcae2c971feda1aecdc3e2e6
Alberto <alberto.peruzzu@gmail.com>
2025-11-26 16:38:41 +0100

    db-path

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
+import os
 from sqlmodel import create_engine, SQLModel, Session
 
-engine = create_engine("sqlite:///./database.db")
-def create_db_and_tables(): SQLModel.metadata.create_all(engine)
+os.makedirs("data", exist_ok=True)
+
+engine = create_engine("sqlite:///./data/database.db")
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 def get_session():
     with Session(engine) as s:
         yield s
