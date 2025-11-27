10e0694acf71df713a231e13b5a277aceca515ba
Alberto <alberto.peruzzu@gmail.com>
2025-11-27 15:26:42 +0100

    cors-url

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://runasone.onrender.com",
-    "random"
+    "https://runasone-backend.onrender.com"
 ]
 
 app.add_middleware(
