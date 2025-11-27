26af35f5567b4553228142c464f52062580cb390
Alberto <alberto.peruzzu@gmail.com>
2025-11-27 13:24:26 +0100

    added-env-var-endpoint

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 const API_BASE_URL =
-  import.meta.env.PROD ? window.location.origin : "http://localhost:8000";
+  import.meta.env.PROD ? import.meta.env.VITE_API_BASE_URL : "http://localhost:8000";
 export default API_BASE_URL;
