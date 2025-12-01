4cbfe3a0fb3cc420e6f18be334ca512ef109cbb6
Alberto <alberto.peruzzu@gmail.com>
2025-12-01 11:35:52 +0100

    fix-fe-fallback

--- app/main.py
+++ app/main.py
@@ -95,7 +95,8 @@ if os.getenv("RENDER") == "true":
     
     frontend_dir = os.path.join(os.path.dirname(__file__), "..", "frontend", "dist")
 
-    app.mount("/", StaticFiles(directory=frontend_dir, html=True), name="frontend")
+    # app.mount("/", StaticFiles(directory=frontend_dir, html=True), name="frontend")
+    app.mount("/assets", StaticFiles(directory=os.path.join(frontend_dir, "assets")), name="assets")
 
     @app.get("/{path:path}", include_in_schema=False)
     async def spa_fallback(path: str):
